6bbb1f64569d09073057454985ae1eccecca52f5
Nathan Minkhorst <nathanminkhorst@gmail.com>
2022-05-02 17:23:31 +0200

    push

---
+++ app.js
@@ -0,0 +1,29 @@
+const cardEl = document.querySelector('.card')
+
+async function getAdvice() {
+    const response = await fetch ('https://api.adviceslip.com/advice')
+    const data = await response.json()
+    const advice = data.slip
+    console.log(advice)
+    renderAdvice(advice)
+}
+
+function renderAdvice(data) {
+    cardEl.innerHTML = 
+    `
+    <h3 class="card__title">Advice #${data.id}</h3>
+    <h1 class="card__advice">"${data.advice}"
+    </h1>
+    <div class="pattern__divider">
+        <i class="divider__icon"><svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg></i>
+    </div>
+    <div class="pattern__divider--mobile">
+                    <i class="divider__icon"><svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg></i>
+    </div>
+    <button class="card__btn" onclick="getAdvice()">
+        <i class="dice__icon"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg></i>
+    </button>
+    `
+}
+
+getAdvice()
